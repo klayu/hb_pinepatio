5a35767133c561da79bc2f45d7d15d51cdb254d4
klayu <manuja.corp@gmail.com>
2023-10-13 23:11:57 -0700

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Deleting old publication"
+rm -f hugo.toml
+rm -rf publicTmp
+mkdir publicTmp
+
+echo "Generating site"
+npm run build
+echo "Copying to gh-pages"
+# echo 'www.i94card.com' >> publicTmp/CNAME
+cp -a publicTmp/. ../gh-pages
+# cd ../gh-pages
+cd ../gh-pages && git add --all && git commit -m "gh-pages branch `date`"
+echo "Pushing to github gh-pages branch"
+git push origin gh-pages
+# HUGO_ENV=production  hugo # -t "ananke"
+
+
+echo "Updating builder branch"
+cd ../builder && rm -rf publicTmp && git add --all && git commit -m "Saving to builder branch"
+
+git push
